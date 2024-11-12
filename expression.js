f80d8f5a7e658a4d5c40cf87d09ad1588173354d
kadir-zeren <k4dzeren@gmail.com>
2024-11-12 22:40:28 +0300

    expression

--- expression.js
+++ expression.js
@@ -6,3 +6,28 @@ console.log(" **** 2-EXPRESSION **** ");
 
 //* ORNEK1:
 // ************************************************ /
+yazdirMerhaba(); //? hoisting oluyor (var değişkenleri)
+
+function yazdirMerhaba() {
+  console.log("Merhaba FS14");
+}
+
+yazdirMerhaba();
+
+//*-----------------------
+//? Declarasyon (Function Expression)
+const yazdir = function (message) {
+  console.log(message);
+};
+
+yazdir("Hallo FS14"); //? invoke
+
+//* ORNEK2:
+// ********************************************* /
+
+let oddEven = function (number) {
+  return number % 2 === 0 ? "EVEN" : "ODD";
+};
+
+console.log(oddEven(5));
+console.log(oddEven(2));
